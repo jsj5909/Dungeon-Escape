2c2db7c2ebbd3649ebc70e9150d87a2283dc534e
jsj5909 <jsj5909@hotmail.com>
2021-09-17 15:00:01 -0700

    Spider start

--- Assets/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Enemy/Spider.cs
+++ Assets/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Enemy/Spider.cs
@@ -4,8 +4,62 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
+    private bool _movingtoA;
+
+    private Animator _animator;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private Vector3 _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+
     public override void Update()
     {
-        throw new System.NotImplementedException();
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            return;
+
+
+
+        Movement();
+
+
+    }
+
+    private void Movement()
+    {
+        if (_target == pointA.position)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+
+        if (transform.position == pointA.position)
+        {
+            _target = pointB.position;
+            _animator.SetTrigger("Idle");
+
+        }
+        else
+        if (transform.position == pointB.position)
+        {
+            _target = pointA.position;
+            _animator.SetTrigger("Idle");
+
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, _target, speed * Time.deltaTime);
     }
 }
