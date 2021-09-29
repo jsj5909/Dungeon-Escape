cba885c849cfd87833f85f7ecab38150040090b4
jsj5909 <jsj5909@hotmail.com>
2021-09-29 15:07:08 -0700

    Spider Attack Complete

--- Assets/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Enemy/Spider.cs
+++ Assets/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Enemy/Spider.cs
@@ -6,16 +6,32 @@ public class Spider : Enemy,IDamageable
 {
     public int Health { get ; set ; }
 
+    public GameObject acidEffectPrefab;
    
     public override void Init()
     {
         base.Init();
+
+        Health = base.health;
     }
+
+    public override void Movement()
+    {
+       
+    }
+
 
     public void Damage()
     {
-        throw new System.NotImplementedException();
+        Health--;
+        
+        if (Health < 1)
+            Destroy(this.gameObject);
     }
 
+    public void Attack()
+    {
+        Instantiate(acidEffectPrefab, transform.position, Quaternion.identity);
+    }
 
 }
---
+++ Assets/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Enemy/SpiderAnimationEvent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAnimationEvent : MonoBehaviour
+{
+
+    private Spider _spider;
+
+    private void Start()
+    {
+        _spider = transform.parent.GetComponent<Spider>();
+    }
+
+    public void Fire()
+    {
+       
+        Debug.Log("SPider should fire");
+
+        _spider.Attack();
+    }
+
+}
